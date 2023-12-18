cbd679998de62cdbe1fc895c84b8a9cd5ae92457
Vinaymhaske <119729473+Vinaymhaske@users.noreply.github.com>
2023-12-18 18:53:32 +0530

    Add files via upload

---
+++ springbootcurdapi/src/main/java/amisha/springbootcurd/springbootcurdapi/SpringbootcurdapiApplication.java
@@ -0,0 +1,15 @@
+package amisha.springbootcurd.springbootcurdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcurdapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcurdapiApplication.class, args);
+		
+		System.out.println("helloooo SpringBoot...");
+	}
+
+}
---
+++ springbootcurdapi/src/test/java/amisha/springbootcurd/springbootcurdapi/SpringbootcurdapiApplicationTests.java
@@ -0,0 +1,13 @@
+package amisha.springbootcurd.springbootcurdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcurdapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
